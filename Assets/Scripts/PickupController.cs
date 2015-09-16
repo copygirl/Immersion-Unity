dfb46e64abbfca4b3866eebef39ecffe4edc317d
copygirl <halfnerd.copygirl@gmail.com>
2015-09-16 13:25:22 +0200

    Update PickupController

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -1,34 +1,41 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Equipment))]
 public class PickupController : MonoBehaviour {
 
 	public float pickupRange = 2.0f;
 
-	public GameObject rightHand;
-	public GameObject leftHand;
+	public GameObject rightHandAttachment;
+	public GameObject leftHandAttachment;
 
-	public Item rightHandItem { get; set; }
-	public Item leftHandItem { get; set; }
+	public EquipmentSlot rightHand { get; private set; }
+	public EquipmentSlot leftHand { get; private set; }
 	
+	Equipment _equipment;
 	CameraController _camera;
 
-
 	void Start() {
+		_equipment = GetComponent<Equipment>();
+		rightHand = _equipment.AddSlot(rightHandAttachment, EquipmentRegion.HAND, EquipmentTag.HELD, EquipmentTag.RIGHT);
+		leftHand  = _equipment.AddSlot(leftHandAttachment, EquipmentRegion.HAND, EquipmentTag.HELD, EquipmentTag.LEFT);
 		_camera = Camera.main.GetComponent<CameraController>();
 	}
 	
 	void Update() {
 		var drop = Input.GetButton("Drop");
+
 		var right = Input.GetButton("Right Hand");
 		var rightPressed = Input.GetButtonDown("Right Hand");
 		var rightReleased = Input.GetButtonUp("Right Hand");
 
 		Item highlightedItem = null;
 
-		if (rightHandItem != null) {
+		if (rightHand.occupied) {
 			if (drop && rightPressed) {
-				rightHandItem.Drop((Vector3.up + _camera.transform.forward) * 200.0f);
-				rightHandItem = null;
+				var item = rightHand.item;
+				rightHand.Unequip();
+				item.OnDrop();
+				item.GetComponent<Rigidbody>().AddForce((Vector3.up + _camera.transform.forward) * 200.0f);
 			}
 		} else {
 			RaycastHit hitInfo;
@@ -44,9 +51,9 @@ public class PickupController : MonoBehaviour {
 			}
 		}
 
-		if ((rightHandItem == null) && (highlightedItem != null) && rightPressed) {
-			highlightedItem.PickUp(gameObject, rightHand);
-			rightHandItem = highlightedItem;
+		if (!rightHand.occupied && (highlightedItem != null) && rightPressed) {
+			highlightedItem.OnPickup();
+			rightHand.Equip(highlightedItem);
 		}
 	}
 
