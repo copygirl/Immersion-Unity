bef1bb43b92763a1c3d4551cf0dcb30774a5a55f
copygirl <halfnerd.copygirl@gmail.com>
2015-09-22 06:55:09 +0200

    Move EmptyBlock to its own file
    Also changed type to class (instead of struct) and overridden ToString,
    Equals and GetHashCode

---
+++ Assets/Scripts/World/Blocks/EmptyBlock.cs
@@ -0,0 +1,48 @@
+
+/// <summary> Empty implementation of IBlock interface.
+///           References a block outside the boundary of the IBlockStorage.
+///           Returns AIR as its material, setting the material does nothing. </summary>
+public class EmptyBlock : IBlock {
+	
+	public IBlockStorage storage { get; private set; }
+	
+	public BlockPos position { get; private set; }
+
+
+	public BlockMaterial material {
+		get { return BlockMaterial.AIR; }
+		set { /* No operation */ }
+	}
+
+	public int amount {
+		get { return BlockData.MAX_AMOUNT; }
+		set { /* No operation */ }
+	}
+
+
+	public EmptyBlock(IBlockStorage storage, BlockPos pos) : this() {
+		this.storage = storage;
+		this.position = pos;
+	}
+
+
+	#region ToString, Equals and GetHashCode
+
+	public override string ToString() {
+		return string.Format("[Block {0}, {1}, <EMPTY>]", position, material);
+	}
+	
+	public override bool Equals(object obj) {
+		var block = (obj as IBlock);
+		return ((block != null) &&
+		        (block.storage == storage) &&
+		        (block.position == position));
+	}
+	
+	public override int GetHashCode() {
+		return HashHelper.GetHashCode(storage, position);
+	}
+
+	#endregion
+	
+}
--- Assets/Scripts/World/Blocks/IBlock.cs
+++ Assets/Scripts/World/Blocks/IBlock.cs
@@ -19,34 +19,6 @@ public interface IBlock {
 	
 }
 
-/// <summary> Empty implementation of IBlock interface.
-///           References a block outside the boundary of the IBlockStorage.
-///           Returns AIR as its material, setting the material does nothing. </summary>
-public struct EmptyBlock : IBlock {
-	
-	public IBlockStorage storage { get; private set; }
-	
-	public BlockPos position { get; private set; }
-
-
-	public BlockMaterial material {
-		get { return BlockMaterial.AIR; }
-		set { /* No operation */ }
-	}
-
-	public int amount {
-		get { return 4; }
-		set { /* No operation */ }
-	}
-
-
-	public EmptyBlock(IBlockStorage storage, BlockPos pos) : this() {
-		this.storage = storage;
-		this.position = pos;
-	}
-	
-}
-
 public static class BlockExtensions {
 
 	/// <summary> Returns another block from the same
