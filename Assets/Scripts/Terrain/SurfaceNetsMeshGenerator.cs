21d9db08b6a3606d985f2d78793200865bb20c1b
copygirl <halfnerd.copygirl@gmail.com>
2015-09-22 05:43:23 +0200

    Update SurfaceNetsMeshGenerator
    Now uses BlockData input, faster IRawBlockAccess and has proper
    smoothing

--- Assets/Scripts/Terrain/SurfaceNetsMeshGenerator.cs
+++ Assets/Scripts/Terrain/SurfaceNetsMeshGenerator.cs
@@ -47,42 +47,38 @@ public class SurfaceNetsMeshGenerator {
 	}
 
 
-	public Mesh Generate(Mesh mesh, IBlockStorage storage) {
-
-		var region = storage.region.Expand(1);
+	public Mesh Generate(Mesh mesh, Terrain terrain) {
 
 		var vertices = new List<Vector3>();
 		var normals  = new List<Vector3>();
 		var colors   = new List<Color>();
 		var indices  = new List<int>();
 
+		var n = 0;
 		var pos = new int[3];
-		var R = new int[]{ 1, (region.width + 1), (region.width + 1) * (region.depth + 1) };
-		var grid = new IBlock[8];
+		var R = new int[]{ 1, (terrain.width + 1), (terrain.width + 1) * (terrain.depth + 1) };
+		var grid = new BlockData[8];
 		var bufNo = 1;
 
 		var vertexBuffer = new Vector3[R[2] * 2];
+		var offset = new Vector3(terrain.width / 2.0F, terrain.depth / 2.0F, terrain.height / 2.0F);
 		
-		for (pos[2] = 0; pos[2] < region.height - 1; pos[2]++, bufNo ^= 1, R[2] = -R[2]) {
+		for (pos[2] = 0; pos[2] < terrain.height - 1; pos[2]++, n += terrain.width, bufNo ^= 1, R[2] = -R[2]) {
 
 			// m is the pointer into the buffer we are going to use. 
-			var m = 1 + (region.width + 1) * (1 + bufNo * (region.depth + 1));
+			var m = 1 + (terrain.width + 1) * (1 + bufNo * (terrain.depth + 1));
 
-			for (pos[1] = 0; pos[1] < region.depth - 1; pos[1]++, m += 2)
-			for (pos[0] = 0; pos[0] < region.width - 1; pos[0]++, m++) {
+			for (pos[1] = 0; pos[1] < terrain.depth - 1; pos[1]++, n++, m += 2)
+			for (pos[0] = 0; pos[0] < terrain.width - 1; pos[0]++, n++, m++) {
 				
 				// Read in 8 field values around this vertex and store them in an array.
 				// Also calculate 8-bit mask, like in marching cubes, so we can speed up sign checks later.
-				var mask = 0;
-				var g = 0;
-				for (var z = 0; z < 2; z++)
-				for (var y = 0; y < 2; y++)
-				for (var x = 0; x < 2; x++, g++) {
-					var blockPos = new BlockPos(region.start.x + pos[0] + x,
-					                            region.start.y + pos[1] + y,
-					                            region.start.z + pos[2] + z);
-					grid[g] = storage[blockPos];
-					mask |= !grid[g].material.solid ? (1 << g) : 0;
+				int mask = 0, g = 0, idx = n;
+				for (var z = 0; z < 2; z++, idx += terrain.width * (terrain.depth - 2))
+				for (var y = 0; y < 2; y++, idx += (terrain.width - 2))
+				for (var x = 0; x < 2; x++, g++, idx++) {
+					grid[g] = terrain.blockData[idx];
+					mask |= grid[g].isSolid ? (1 << g) : 0;
 				}
 				
 				// Check for early termination if cell does not intersect boundary.
@@ -105,15 +101,15 @@ public class SurfaceNetsMeshGenerator {
 					eCount++;
 					
 					// Now find the point of intersection.
-					var e0 = _cubeEdges[i << 1];           // Unpack vertices.
+					var e0 = _cubeEdges[i << 1];       // Unpack vertices.
 					var e1 = _cubeEdges[(i << 1) + 1];
-					var g0 = (grid[e0].amount - 2) / 3.0F; // Unpack grid values.
-					var g1 = (grid[e1].amount - 2) / 3.0F;
-					var t  = g0 - g1;                      // Compute point of intersection.
+					var g0 = GetGridValue(grid[e0]);   // Unpack grid values.
+					var g1 = GetGridValue(grid[e1]);
+					var t  = g0 - g1;                  // Compute point of intersection.
 
 					if (Mathf.Abs(t) <= 1e-6)
 						continue;
-					t = g0 / t;
+					// t = g0 / t;
 					
 					// Interpolate vertices and add up intersections (this can be done without multiplying).
 					for (int j = 0, k = 1; j < 3; j++, k <<= 1) {
@@ -130,7 +126,7 @@ public class SurfaceNetsMeshGenerator {
 					v[i] = pos[i] + s * v[i];
 				
 				// Add vertex to buffer.
-				vertexBuffer[m] = v;
+				vertexBuffer[m] = v - offset;
 				
 				// Now we need to add faces together, to do this we just loop over 3 basic components.
 				for (var i = 0; i < 3; i++) {
@@ -143,7 +139,7 @@ public class SurfaceNetsMeshGenerator {
 					var iv = (i + 2) % 3;
 					
 					// If we are on a boundary, skip it.
-					if ((pos[iu] < 0) || (pos[iv] < 0))
+					if ((pos[iu] <= 0) || (pos[iv] <= 0))
 						continue;
 					
 					// Otherwise, look up adjacent edges in buffer.
@@ -159,12 +155,12 @@ public class SurfaceNetsMeshGenerator {
 					var normal2 = Vector3.Cross(v3 - v1, v4 - v1).normalized;
 
 					// Remember to flip orientation depending on the sign of the corner.
-					if ((mask & 1) == 0) {
-						var color = grid[0].material.color;
+					if ((mask & 1) != 0) {
+						var color = terrain._idToMaterial[grid[0].material].color;
 						AddFlatTriangle(vertices, normals, colors, indices, v1, v2, v3, normal1, color);
 						AddFlatTriangle(vertices, normals, colors, indices, v1, v3, v4, normal2, color);
 					} else {
-						var color = grid[1 << i].material.color;
+						var color = terrain._idToMaterial[grid[1 << i].material].color;
 						AddFlatTriangle(vertices, normals, colors, indices, v4, v2, v1, -normal1, color);
 						AddFlatTriangle(vertices, normals, colors, indices, v4, v3, v2, -normal2, color);
 					}
@@ -181,6 +177,11 @@ public class SurfaceNetsMeshGenerator {
 
 		return mesh;
 
+	}
+
+
+	static float GetGridValue(BlockData block) {
+		return (block.isSolid ? (float)block.amount / BlockData.MAX_AMOUNT : 0.0F);
 	}
 
 
