2d9acec45297c22b17aecda5a11399f3df497882
copygirl <halfnerd.copygirl@gmail.com>
2015-09-30 17:17:19 +0200

    Use Concat in EnumerableExtensions

--- Assets/Scripts/Utility/EnumerableExtensions.cs
+++ Assets/Scripts/Utility/EnumerableExtensions.cs
@@ -1,23 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public static class EnumerableExtensions {
 
 	/// <summary> Returns an enumerable that, when iterated, returns the
 	///           provided elements before the source enumerable. </summary>
 	public static IEnumerable<T> Precede<T>(this IEnumerable<T> enumerable, params T[] elements) {
-		foreach (var element in elements)
-			yield return element;
-		foreach (var element in enumerable)
-			yield return element;
+		return elements.Concat(enumerable);
 	}
 
 	/// <summary> Returns an enumerable that, when iterated, returns the
 	///           provided elements after the source enumerable. </summary>
 	public static IEnumerable<T> Follow<T>(this IEnumerable<T> enumerable, params T[] elements) {
-		foreach (var element in enumerable)
-			yield return element;
-		foreach (var element in elements)
-			yield return element;
+		return enumerable.Concat(elements);
 	}
 
 }
