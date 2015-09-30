1c47cbbc3668d372e6ecd10f4a52a3661c8b536d
copygirl <halfnerd.copygirl@gmail.com>
2015-09-30 17:18:41 +0200

    Add IEnumerable.Contains(enumerable, elements...)

--- Assets/Scripts/Utility/EnumerableExtensions.cs
+++ Assets/Scripts/Utility/EnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,4 +16,19 @@ public static class EnumerableExtensions {
 		return enumerable.Concat(elements);
 	}
 
+
+	/// <summary> Returns if the source enumerable contains all elements in the specified enumerable. </summary>
+	public static bool Contains<T>(this IEnumerable<T> enumerable, IEnumerable<T> elements) {
+		foreach (var element in elements)
+			if (!Enumerable.Contains(enumerable, element))
+				return false;
+		return true;
+	}
+	/// <summary> Returns if the enumerable contains all the specified elements. </summary>
+	public static bool Contains<T>(this IEnumerable<T> enumerable, params T[] elements) {
+		if (elements.Length <= 0)
+			throw new ArgumentException("elements is empty", "elements");
+		return enumerable.Contains((IEnumerable<T>)elements);
+	}
+	
 }
