c81a8c29c5a6a4ee5c601657e3d22b9a8e3e2baa
copygirl <halfnerd.copygirl@gmail.com>
2015-09-30 14:30:43 +0200

    Add ToolType and support in Tool

--- Assets/Scripts/Gameplay/Items/Tool.cs
+++ Assets/Scripts/Gameplay/Items/Tool.cs
@@ -1,17 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-/// <summary> Extension component for items that
-///           may be used as tools or weapons. </summary>
+/// <summary> Extension component for items that ay be used as tools or weapons. </summary>
 [RequireComponent(typeof(Item))]
 public class Tool : MonoBehaviour {
 
 	Item _item;
+	
+	[SerializeField]
+	public float[] toolEffectiveness = new float[Enum.GetValues(typeof(ToolType)).Length - 1];
 
 
 	/// <summary> Gets or sets the weight of the tool in kilograms. </summary>
 	public float weight {
 		get { return _item.weight; }
 		set { _item.weight = value; }
+	}
+
+	/// <summary> Gets or sets the raw effectiveness of
+	///           this tool for the specified tool type. </summary>
+	public float this[ToolType type] {
+		get { return ((Enum.IsDefined(typeof(ToolType), type) && (type != ToolType.None))
+			               ? toolEffectiveness[(int)type - 1] : 0.0F); }
+		set {
+			if ((value < 0.0F) || (value > 1.0F))
+				throw new ArgumentOutOfRangeException("value", value,
+					"Effectiveness is not a valid value (0.0 to 1.0)");
+			if (!Enum.IsDefined(typeof(ToolType), type) || (type == ToolType.None))
+				throw new ArgumentException(string.Format(
+					"'{0}' is not a valid ToolType", type), "type");
+			toolEffectiveness[(int)type - 1] = value;
+		}
 	}
 
 
@@ -24,4 +46,24 @@ public class Tool : MonoBehaviour {
 		return ((tool != null) ? tool._item : null);
 	}
 
+}
+
+[CustomEditor(typeof(Tool))]
+public class ToolEditor : Editor {
+
+	bool _visible = false;
+
+	public override void OnInspectorGUI() {
+		Tool tool = (Tool)target;
+
+		if (_visible = EditorGUILayout.Foldout(_visible, "Tool Effectiveness")) {
+			EditorGUI.indentLevel++;
+			for (var i = 0; i < tool.toolEffectiveness.Length; i++) {
+				var type = (ToolType)(i + 1);
+				tool[type] = EditorGUILayout.Slider(type.ToString(), tool[type], 0.0F, 1.0F);
+			}
+			EditorGUI.indentLevel--;
+		}
+	}
+	
 }
---
+++ Assets/Scripts/Gameplay/Items/ToolType.cs
@@ -0,0 +1,23 @@
+
+public enum ToolType {
+
+	None = 0,
+
+	/// <summary> Smashes things into pieces. Useful for
+	///           early-game crafting and resource gathering. </summary>
+	Hammer,
+
+	/// <summary> Chops plants and trees down and into smaller pieces. Useful
+	///           for early- to mid-game resource gathering and some crafting. </summary>
+	Axe,
+
+	/// <summary> Breaks apart harder materials for gathering raw stone,
+	///           ores and other minerals to progress in mid- to late-game. </summary>
+	Pickaxe,
+
+	/// <summary> Slices or seperates things into parts,
+	///           like meat or raw hide from animals. </summary>
+	Blade
+
+}
+
